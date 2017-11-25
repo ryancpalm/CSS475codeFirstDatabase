cb2a6b67f4c89f633dd135894649969937c50ca5
Trystan-C <trys.cannon@gmail.com>
2017-11-25 14:07:12 -0800

    Update attriubte validation.

--- medDatabase.Domain/Models/Employee.cs
+++ medDatabase.Domain/Models/Employee.cs
@@ -5,10 +5,12 @@ namespace medDatabase.Domain.Models
     public class Employee
     {
         [Key]
+        [Required]
         public int Id { get; set; }
 
         // No '-'
         [StringLength(9)]
+        [Required]
         public string Ssn { get; set; }
 
         // Will continue adding attributes later...
