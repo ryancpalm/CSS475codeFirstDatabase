34fb44a2065eb9edcc71e6cc4e4acdf0344ea0b5
Trystan-C <trys.cannon@gmail.com>
2017-11-25 14:30:19 -0800

    Add domain unit tests

---
+++ medDatabase.Domain.Tests/MedicalDatabaseContextTests.cs
@@ -0,0 +1,38 @@
+﻿using medDatabase.Domain.Contexts;
+using NUnit.Framework;
+
+namespace medDatabase.Domain.Tests
+{
+    [TestFixture]
+    public class MedicalDatabaseContextTests
+    {
+        private MedicalDatabaseContext _medDbContext;
+
+        [SetUp]
+        public void BeforeEach()
+        {
+            _medDbContext = new MedicalDatabaseContext();
+            _medDbContext.Database.CreateIfNotExists();
+        }
+
+        [Test]
+        public void MedicalDatabaseContextCanConnectToServer()
+        {
+            // Act
+            var databaseConnection = _medDbContext.Database.Connection;
+
+            // Assert
+            Assert.That(databaseConnection, Is.Not.Null);
+        }
+
+        [Test]
+        public void MedicalDatabaseContextDatabaseExists()
+        {
+            // Act
+            var dbExists = _medDbContext.Database.Exists();
+
+            // Assert
+            Assert.That(dbExists, Is.True);
+        }
+    }
+}
