86da4b317417d6c86b104203bf0ad17f07f211b9
lightinglark <ryancpalm@gmail.com>
2017-12-08 14:47:41 -0800

    Basic web functionality

---
+++ medDatabase/Controllers/DoctorSpecialtiesController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using medDatabase.Models;
+
+namespace medDatabase.Controllers
+{
+    public class DoctorSpecialtiesController : Controller
+    {
+        private Medical_DatabaseEntities db = new Medical_DatabaseEntities();
+
+        // GET: DoctorSpecialties
+        public ActionResult Index()
+        {
+            return View(db.DoctorSpecialties.ToList());
+        }
+
+        // GET: DoctorSpecialties/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            DoctorSpecialty doctorSpecialty = db.DoctorSpecialties.Find(id);
+            if (doctorSpecialty == null)
+            {
+                return HttpNotFound();
+            }
+            return View(doctorSpecialty);
+        }
+
+        // GET: DoctorSpecialties/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: DoctorSpecialties/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Name")] DoctorSpecialty doctorSpecialty)
+        {
+            if (ModelState.IsValid)
+            {
+                db.DoctorSpecialties.Add(doctorSpecialty);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(doctorSpecialty);
+        }
+
+        // GET: DoctorSpecialties/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            DoctorSpecialty doctorSpecialty = db.DoctorSpecialties.Find(id);
+            if (doctorSpecialty == null)
+            {
+                return HttpNotFound();
+            }
+            return View(doctorSpecialty);
+        }
+
+        // POST: DoctorSpecialties/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Name")] DoctorSpecialty doctorSpecialty)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(doctorSpecialty).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(doctorSpecialty);
+        }
+
+        // GET: DoctorSpecialties/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            DoctorSpecialty doctorSpecialty = db.DoctorSpecialties.Find(id);
+            if (doctorSpecialty == null)
+            {
+                return HttpNotFound();
+            }
+            return View(doctorSpecialty);
+        }
+
+        // POST: DoctorSpecialties/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            DoctorSpecialty doctorSpecialty = db.DoctorSpecialties.Find(id);
+            db.DoctorSpecialties.Remove(doctorSpecialty);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ medDatabase/Controllers/PatientsController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using medDatabase.Models;
+
+namespace medDatabase.Controllers
+{
+    public class PatientsController : Controller
+    {
+        private Medical_DatabaseEntities db = new Medical_DatabaseEntities();
+
+        // GET: Patients
+        public ActionResult Index()
+        {
+            var patients = db.Patients.Include(p => p.Room);
+            return View(patients.ToList());
+        }
+
+        // GET: Patients/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Patient patient = db.Patients.Find(id);
+            if (patient == null)
+            {
+                return HttpNotFound();
+            }
+            return View(patient);
+        }
+
+        // GET: Patients/Create
+        public ActionResult Create()
+        {
+            ViewBag.RoomId = new SelectList(db.Rooms, "Id", "Id");
+            return View();
+        }
+
+        // POST: Patients/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Ssn,RoomId,FirstName,LastName,Age,Gender,AdmissionDate,DischargeDate")] Patient patient)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Patients.Add(patient);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.RoomId = new SelectList(db.Rooms, "Id", "Id", patient.RoomId);
+            return View(patient);
+        }
+
+        // GET: Patients/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Patient patient = db.Patients.Find(id);
+            if (patient == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.RoomId = new SelectList(db.Rooms, "Id", "Id", patient.RoomId);
+            return View(patient);
+        }
+
+        // POST: Patients/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Ssn,RoomId,FirstName,LastName,Age,Gender,AdmissionDate,DischargeDate")] Patient patient)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(patient).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.RoomId = new SelectList(db.Rooms, "Id", "Id", patient.RoomId);
+            return View(patient);
+        }
+
+        // GET: Patients/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Patient patient = db.Patients.Find(id);
+            if (patient == null)
+            {
+                return HttpNotFound();
+            }
+            return View(patient);
+        }
+
+        // POST: Patients/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Patient patient = db.Patients.Find(id);
+            db.Patients.Remove(patient);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
