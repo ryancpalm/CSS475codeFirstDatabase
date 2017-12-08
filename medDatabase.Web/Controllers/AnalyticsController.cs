c5c88e635c6015eda9ac134eb716f755464ed469
Trystan-C <trys.cannon@gmail.com>
2017-12-07 17:11:33 -0800

    Add Analytics Controller; Set Login as Homepage

---
+++ medDatabase.Web/Controllers/AnalyticsController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace medDatabase.Web.Controllers
+{
+    [Authorize]
+    public class AnalyticsController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // Calculates the average stay period for patients with a given condition.
+        // Uses joins on medical history, illness, and patient.
+        public ActionResult StayPeriod(string illness)
+        {
+            throw new NotImplementedException();
+        }
+
+        // Calculates the average recovery period for patients with a given condition
+        // using a given medicaiton. Joins medical history, illness, and patient.
+        public ActionResult RecoveryTime(string illness, string medication)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
